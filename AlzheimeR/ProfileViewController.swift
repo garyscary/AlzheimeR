06813d6a2418c1bfadd6cd2258a389ca78ca1679
Rahman <prahman@dhcp-6c-8d-c1-41-54-7f.ubcvisitor.wifi.ubc.ca>
2018-01-14 11:49:45 -0700

    created second screen - profile

---
+++ AlzheimeR/ProfileViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ProfileViewController.swift
+//  AlzheimeR
+//
+//  Created by Rahman, Parash on 1/14/18.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var relationTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
